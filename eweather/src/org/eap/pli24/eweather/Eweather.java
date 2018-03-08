e5f380205766db8bbc53ede3a9645dd44e1336e5
saggelopoulos <37022892+saggelopoulos@users.noreply.github.com>
2018-03-08 16:47:08 +0200

    visual view

--- eweather/src/org/eap/pli24/eweather/Eweather.java
+++ eweather/src/org/eap/pli24/eweather/Eweather.java
@@ -5,9 +5,13 @@
  */
 package org.eap.pli24.eweather;
 
+import java.awt.BorderLayout;
 import java.util.List;
+import javax.swing.JFrame;
 import org.eap.pli24.eweather.controller.Controller;
 import org.eap.pli24.eweather.model.WeatherForecast;
+import org.eap.pli24.eweather.view.ActualWeather;
+import org.eap.pli24.eweather.view.Central;
 
 /**
  *
@@ -15,6 +19,11 @@ import org.eap.pli24.eweather.model.WeatherForecast;
  */
 public class Eweather {
 
+    
+    private static ActualWeather actualWeather;
+    
+    private static Central central ;
+    
     /**
      * @param args the command line arguments
      */
@@ -25,8 +34,26 @@ public class Eweather {
         Controller controller = new Controller();
         
         List<WeatherForecast>  minMaxTemprature = controller.getMinMaxTemprature();
-       
-   
+               
+        JFrame frame = new JFrame("weather ");
+        frame.setSize(800,600);
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // as per your requirement.
+        central = new Central();
+       // actualWeather = new ActualWeather();
+      //  actualWeather.setSize(600,500);
+        //actualWeather.setBounds(1, 1, 600, 500);
+        //actualWeather.setVisible(true);
+          
+      
+        //central.MainPanel.add(actualWeather ,BorderLayout.CENTER);
+      
+        
+          frame.add(central, BorderLayout.CENTER);
+        frame.setVisible(true);
+
+        
    
     
     }
