61de40f59e96bf33a4c6c2811a158d130c3ae569
Mike Gikas <mikegikas@users.noreply.github.com>
2018-03-22 16:58:55 +0200

    update 0.6

--- eweather/src/org/eap/pli24/eweather/view/ImageRenderer.java
+++ eweather/src/org/eap/pli24/eweather/view/ImageRenderer.java
@@ -16,29 +16,22 @@ import javax.swing.table.DefaultTableCellRenderer;
  * @author Αυγερινός Παναγιώτης
  * @author Γκίκας Μιχαήλ
  */
-public class ImageRenderer extends DefaultTableCellRenderer
-{
+public class ImageRenderer extends DefaultTableCellRenderer{
     @Override
-    public Component getTableCellRendererComponent(JTable table, Object value,boolean isSelected, boolean hasFocus, int row, int column)
-    {
+    public Component getTableCellRendererComponent(JTable table, Object value,boolean isSelected, boolean hasFocus, int row, int column){
         JLabel lbl =((JLabel)super.getTableCellRendererComponent(table,"",isSelected,hasFocus,row,column));
     
         URL url = null;
-         try
-         {
-             if (value != null)
-             {
-                     url = new URL((String)value);
-                    lbl.setIcon(new ImageIcon(url));
-             }
-             else
-             {
-                 lbl.setIcon(null);
-             }
-         } catch (MalformedURLException ex)
-         {
-             //Logger.getLogger(ImageRenderer.class.getName()).log(Level.SEVERE, null, ex);
-         }
+        try{
+            if (value != null){
+                url = new URL((String)value);
+                lbl.setIcon(new ImageIcon(url));
+            }else{
+                lbl.setIcon(null);
+            }
+        } catch (MalformedURLException ex){
+            //Logger.getLogger(ImageRenderer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     return lbl;
-  }
+    }
 }
